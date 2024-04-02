75b81cc1cc7b1cc837683bde73b2d53a03874406
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-02 20:47:37 +0300

    feat: добавил DAO и перенёс туда логику реализации репозитория

--- src/main/java/com/example/services/CurrencyService.java
+++ src/main/java/com/example/services/CurrencyService.java
@@ -1,119 +1,38 @@
 package com.example.services;
 
 import com.example.DTO.CurrencyDTO;
-import com.example.repositories.CurrencyRepository;
-import com.example.util.ConnectionDB;
+import com.example.DAO.CurrencyDAO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-public class CurrencyService implements CurrencyRepository {
-    private static final String FIND_ALL_QUERY = "SELECT * FROM Currencies";
-    private static final String FIND_BY_ID_QUERY = "SELECT * FROM Currencies WHERE id=?";
-    private static final String FIND_BY_CODE_QUERY = "SELECT * FROM Currencies WHERE code=?";
-    private static final String SAVE_QUERY = "INSERT INTO Currencies (code, fullName, sign) VALUES (?, ?, ?)";
-    private static final String UPDATE_QUERY = "UPDATE Currencies SET code=?, fullName=?, sign=? WHERE id=?";
-    private static final String DELETE_QUERY = "DELETE FROM Currencies WHERE id=?";
-    private CurrencyDTO mapResultSetToCurrency(ResultSet resultSet) throws SQLException {
-        int id = resultSet.getInt("id");
-        String code = resultSet.getString("code");
-        String fullName = resultSet.getString("fullName");
-        String sign = resultSet.getString("sign");
+public class CurrencyService {
+    private final CurrencyDAO currencyDAO;
 
-        CurrencyDTO currency = new CurrencyDTO(code, fullName, sign);
-        currency.setId(id);
-        return currency;
+    public CurrencyService() {
+        this.currencyDAO = new CurrencyDAO();
     }
 
-    @Override
     public List<CurrencyDTO> findAll() {
-        List<CurrencyDTO> currencies = new ArrayList<>();
-        try (Connection connection = ConnectionDB.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_ALL_QUERY);
-             ResultSet resultSet = statement.executeQuery()) {
-            while (resultSet.next()) {
-                CurrencyDTO currency = mapResultSetToCurrency(resultSet);
-                currencies.add(currency);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return currencies;
+        return currencyDAO.findAll();
     }
 
-    @Override
     public CurrencyDTO findById(int id) {
-        CurrencyDTO currency = null;
-        try (Connection connection = ConnectionDB.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_QUERY)) {
-            statement.setInt(1, id);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    currency = mapResultSetToCurrency(resultSet);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return currency;
+        return currencyDAO.findById(id);
     }
 
-    @Override
     public CurrencyDTO findByCode(String code) {
-        CurrencyDTO currency = null;
-        try (Connection connection = ConnectionDB.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_BY_CODE_QUERY)) {
-            statement.setString(1, code);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    currency = mapResultSetToCurrency(resultSet);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return currency;
+        return currencyDAO.findByCode(code);
     }
 
-    @Override
     public void save(CurrencyDTO currency) {
-        try (Connection connection = ConnectionDB.getConnection();
-             PreparedStatement statement = connection.prepareStatement(SAVE_QUERY)) {
-            statement.setString(1, currency.getCode());
-            statement.setString(2, currency.getFullName());
-            statement.setString(3, currency.getSign());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        currencyDAO.save(currency);
     }
 
-    @Override
     public void update(CurrencyDTO currency) {
-        try (Connection connection = ConnectionDB.getConnection();
-             PreparedStatement statement = connection.prepareStatement(UPDATE_QUERY)) {
-            statement.setString(1, currency.getCode());
-            statement.setString(2, currency.getFullName());
-            statement.setString(3, currency.getSign());
-            statement.setInt(4, currency.getId()); // Устанавливаем ID валюты для обновления
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        currencyDAO.update(currency);
     }
 
-    @Override
     public void delete(CurrencyDTO currency) {
-        try (Connection connection = ConnectionDB.getConnection();
-             PreparedStatement statement = connection.prepareStatement(DELETE_QUERY)) {
-            statement.setInt(1, currency.getId()); // Устанавливаем ID валюты для удаления
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        currencyDAO.delete(currency);
     }
 }
