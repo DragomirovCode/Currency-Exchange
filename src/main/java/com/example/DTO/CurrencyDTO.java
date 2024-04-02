d12cf6a2c4566c897d7429f3a361cb1d21c9359e
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-02 12:31:14 +0300

    feat: Добавил атрибуты к сущности "Currency", а так же геттеры и сеттеры

--- src/main/java/com/example/DTO/CurrencyDTO.java
+++ src/main/java/com/example/DTO/CurrencyDTO.java
@@ -1,4 +1,46 @@
 package com.example.DTO;
 
 public class CurrencyDTO {
+    private int ID;
+    private String Code;
+    private String FullName;
+    private String Sign;
+
+    public CurrencyDTO(String code, String fullName, String sign) {
+        Code = code;
+        FullName = fullName;
+        Sign = sign;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String code) {
+        Code = code;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String fullName) {
+        FullName = fullName;
+    }
+
+    public String getSign() {
+        return Sign;
+    }
+
+    public void setSign(String sign) {
+        Sign = sign;
+    }
 }
