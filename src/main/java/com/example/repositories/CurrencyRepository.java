ee1044e4b23c99a4c6e955df9f37601d343b4cc0
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-02 17:00:36 +0300

    feat: Добавил в репозиторий "CurrencyRepository" методы: "findAll", "findById", "findByCode", "save", "update", "delete".

--- src/main/java/com/example/repositories/CurrencyRepository.java
+++ src/main/java/com/example/repositories/CurrencyRepository.java
@@ -1,4 +1,14 @@
 package com.example.repositories;
 
+import com.example.DTO.CurrencyDTO;
+
+import java.util.List;
+
 public interface CurrencyRepository {
+    List<CurrencyDTO> findAll();
+    CurrencyDTO findById(int id);
+    CurrencyDTO findByCode(String code);
+    void save(CurrencyDTO currency);
+    void update(CurrencyDTO currency);
+    void delete(CurrencyDTO currency);
 }
