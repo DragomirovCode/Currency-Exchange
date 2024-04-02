906ff38c39074a54637a9d074e3656f4121c5297
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-02 15:04:47 +0300

    feat: Добавил подключение к бд.

--- src/main/java/com/example/util/ConnectionDB.java
+++ src/main/java/com/example/util/ConnectionDB.java
@@ -1,4 +1,24 @@
 package com.example.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class ConnectionDB {
+    private static final String URL = "jdbc:sqlite:./src/main/resources/db/database.db";
+    private static Connection connection;
+
+    // Приватный конструктор, чтобы предотвратить создание экземпляров класса
+    private ConnectionDB() {}
+    public synchronized static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                Class.forName("org.sqlite.JDBC");
+                connection = DriverManager.getConnection(URL);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                throw new SQLException("SQLite JDBC driver not found");
+            }
+        }
+        return connection;
+    }
 }
