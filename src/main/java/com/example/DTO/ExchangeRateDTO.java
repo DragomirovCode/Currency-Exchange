88f7101a9066abd7e7afb9aacef770cad4aaa256
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-02 12:36:44 +0300

    feat: Добавил атрибуты к сущности "ExchangeRate", а так же геттеры и сеттеры

--- src/main/java/com/example/DTO/ExchangeRateDTO.java
+++ src/main/java/com/example/DTO/ExchangeRateDTO.java
@@ -1,4 +1,48 @@
 package com.example.DTO;
 
+import java.math.BigDecimal;
+
 public class ExchangeRateDTO {
+    private int ID;
+    private int BaseCurrencyId;
+    private int TargetCurrencyId;
+    private BigDecimal Rate;
+
+    public ExchangeRateDTO(int baseCurrencyId, int targetCurrencyId, BigDecimal rate) {
+        BaseCurrencyId = baseCurrencyId;
+        TargetCurrencyId = targetCurrencyId;
+        Rate = rate;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getBaseCurrencyId() {
+        return BaseCurrencyId;
+    }
+
+    public void setBaseCurrencyId(int baseCurrencyId) {
+        BaseCurrencyId = baseCurrencyId;
+    }
+
+    public int getTargetCurrencyId() {
+        return TargetCurrencyId;
+    }
+
+    public void setTargetCurrencyId(int targetCurrencyId) {
+        TargetCurrencyId = targetCurrencyId;
+    }
+
+    public BigDecimal getRate() {
+        return Rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        Rate = rate;
+    }
 }
