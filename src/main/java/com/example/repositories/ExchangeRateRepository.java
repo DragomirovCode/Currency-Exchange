ca34bdb010ec3b3054d1a6c879cc59806a1958df
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-02 17:38:15 +0300

    feat: Добавил в репозиторий "ExchangeRateRepository" методы: "findAll", "findById", "findByCurrencyPair", "save", "update", "delete"

--- src/main/java/com/example/repositories/ExchangeRateRepository.java
+++ src/main/java/com/example/repositories/ExchangeRateRepository.java
@@ -1,4 +1,14 @@
 package com.example.repositories;
 
+import com.example.DTO.ExchangeRateDTO;
+
+import java.util.List;
+
 public interface ExchangeRateRepository {
+    List<ExchangeRateDTO> findAll();
+    ExchangeRateDTO findById(int id);
+    ExchangeRateDTO findByCurrencyPair(int baseCurrencyCode, int targetCurrencyCode);
+    void save(ExchangeRateDTO exchangeRate);
+    void update(ExchangeRateDTO exchangeRate);
+    void delete(ExchangeRateDTO exchangeRate);
 }
