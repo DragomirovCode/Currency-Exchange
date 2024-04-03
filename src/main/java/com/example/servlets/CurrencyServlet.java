510963ac055d9fe1b5ff6d6c6c3135e756767c44
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-03 14:08:37 +0300

    feat: Реализовал метод "doGet" в сервлете "CurrencyServlet"

--- src/main/java/com/example/servlets/CurrencyServlet.java
+++ src/main/java/com/example/servlets/CurrencyServlet.java
@@ -1,4 +1,30 @@
 package com.example.servlets;
 
-public class CurrencyServlet {
+import com.example.services.CurrencyService;
+import com.google.gson.Gson;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/currency/*")
+public class CurrencyServlet extends HttpServlet {
+    private final CurrencyService currencyService;
+
+    public CurrencyServlet(CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        Gson gson = new Gson();
+        String json = gson.toJson(currencyService.findAll());
+        resp.getWriter().write(json);
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
 }
