0c52d02477b073791e950ee946980a8622bda8c9
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-12 13:33:05 +0300

    feat: Добавил новый атрибут в findById метод в "CurrencyRepository"

--- src/main/java/com/example/repositories/CurrencyRepository.java
+++ src/main/java/com/example/repositories/CurrencyRepository.java
@@ -2,11 +2,12 @@ package com.example.repositories;
 
 import com.example.dto.CurrencyDTO;
 
+import java.sql.Connection;
 import java.util.List;
 
 public interface CurrencyRepository {
     List<CurrencyDTO> findAll();
-    CurrencyDTO findById(int id);
+    CurrencyDTO findById(int id, Connection connection);
     CurrencyDTO findByCode(String code);
     void save(CurrencyDTO currency);
     void update(CurrencyDTO currency);
