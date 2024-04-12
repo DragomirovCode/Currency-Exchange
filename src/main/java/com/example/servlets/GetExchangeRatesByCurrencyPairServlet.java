f40df9edc7cfe95bd385428cca066e2ffef2be4d
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-12 16:27:55 +0300

    chore: Изменил нейминг

--- src/main/java/com/example/servlets/GetExchangeRatesByCurrencyPairServlet.java
+++ src/main/java/com/example/servlets/GetExchangeRatesByCurrencyPairServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class GetExchangeRatesByIdServlet extends HttpServlet {
+public class GetExchangeRatesByCurrencyPairServlet extends HttpServlet {
     private ExchangeRateService exchangeRateService;
     private CurrencyService currencyService;
 
