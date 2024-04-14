cfeb707a2e0da5ce0f222aa93f4e779f13dc271b
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-14 23:19:29 +0300

    feat: Начал наследоваться от "BaseServletUtils"

--- src/main/java/com/example/servlets/ExchangeRateCalculationServlet.java
+++ src/main/java/com/example/servlets/ExchangeRateCalculationServlet.java
@@ -5,9 +5,9 @@ import com.example.dto.CurrencyDTO;
 import com.example.dto.ExchangeRateDTO;
 import com.example.services.CurrencyService;
 import com.example.services.ExchangeRateService;
+import com.example.util.BaseServletUtils;
 import com.google.gson.Gson;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -15,7 +15,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 
 @WebServlet(name = "ExchangeRateCalculationServlet", urlPatterns = "/exchange")
-public class ExchangeRateCalculationServlet extends HttpServlet {
+public class ExchangeRateCalculationServlet extends BaseServletUtils {
     private ExchangeRateService exchangeRateService;
     private CurrencyService currencyService;
 
