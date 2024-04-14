79674bf0346afe186bee1f4e66c4e808e93cb97e
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-14 22:26:28 +0300

    chore: переименовал класс и добавил в один класс два метода

--- src/main/java/com/example/servlets/CurrencyListAndCreateServlet.java
+++ src/main/java/com/example/servlets/CurrencyListAndCreateServlet.java
@@ -2,22 +2,35 @@ package com.example.servlets;
 
 import com.example.dto.CurrencyDTO;
 import com.example.services.CurrencyService;
+import com.example.util.BaseServletUtils;
 import com.google.gson.Gson;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
-@WebServlet(name = "PostCurrenciesServlet", urlPatterns = "/currencies")
-public class PostCurrenciesServlet extends HttpServlet {
+@WebServlet(name = "CurrencyListAndCreateServlet", urlPatterns = "/currencies")
+public class CurrencyListAndCreateServlet extends BaseServletUtils {
     private CurrencyService currencyService;
 
     @Override
     public void init() {
-        // Здесь вы можете инициализировать ваш сервис, например:
         currencyService = new CurrencyService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        Gson gson = new Gson();
+        try {
+            String json = gson.toJson(currencyService.findAll());
+            resp.getWriter().write(json);
+            resp.setStatus(HttpServletResponse.SC_OK);
+        } catch (Exception e) {
+            handleException(resp, e, "База данных недоступна");
+        }
     }
 
     @Override
