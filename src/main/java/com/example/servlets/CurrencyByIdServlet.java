9507fb5178a1b5f975f871f457d23a158c0d8e5a
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-14 22:51:18 +0300

    feat: Добавил ошибку 500, убрал лишний комментарий, наследовался от "BaseServletUtils"

--- src/main/java/com/example/servlets/CurrencyByIdServlet.java
+++ src/main/java/com/example/servlets/CurrencyByIdServlet.java
@@ -2,8 +2,8 @@ package com.example.servlets;
 
 import com.example.dto.CurrencyDTO;
 import com.example.services.CurrencyService;
+import com.example.util.BaseServletUtils;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -12,12 +12,11 @@ import com.google.gson.Gson;
 import java.io.IOException;
 
 @WebServlet(name = "CurrencyByIdServlet", urlPatterns = "/currency/*")
-public class CurrencyByIdServlet extends HttpServlet {
+public class CurrencyByIdServlet extends BaseServletUtils {
     private CurrencyService currencyService;
 
     @Override
     public void init() {
-        // Здесь вы можете инициализировать ваш сервис, например:
         currencyService = new CurrencyService();
     }
 
@@ -26,24 +25,28 @@ public class CurrencyByIdServlet extends HttpServlet {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
 
-        String pathInfo = req.getPathInfo();
-        String currencyCode = pathInfo.substring(1);
+        try {
+            String pathInfo = req.getPathInfo();
+            String currencyCode = pathInfo.substring(1);
 
-        if (currencyCode.isEmpty()) {
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
+            if (currencyCode.isEmpty()) {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                return;
+            }
+
+            CurrencyDTO currency = currencyService.findByCode(currencyCode);
+
+            if (currency == null) {
+                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
+
+            Gson gson = new Gson();
+            String json = gson.toJson(currency);
+            resp.getWriter().write(json);
+            resp.setStatus(HttpServletResponse.SC_OK);
+        } catch (Exception e){
+            handleException(resp, e, "База данных недоступна");
         }
-
-        CurrencyDTO currency = currencyService.findByCode(currencyCode);
-
-        if (currency == null) {
-            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            return;
-        }
-
-        Gson gson = new Gson();
-        String json = gson.toJson(currency);
-        resp.getWriter().write(json);
-        resp.setStatus(HttpServletResponse.SC_OK);
     }
 }
