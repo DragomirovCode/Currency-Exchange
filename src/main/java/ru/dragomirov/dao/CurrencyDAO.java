f0a3e066b6e196a4085247fa3845b54c998cdd43
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-17 00:52:08 +0300

    chore

--- src/main/java/ru/dragomirov/dao/CurrencyDAO.java
+++ src/main/java/ru/dragomirov/dao/CurrencyDAO.java
@@ -2,7 +2,6 @@ package ru.dragomirov.dao;
 
 import ru.dragomirov.models.Currency;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CurrencyDAO extends CrudDAO<Currency, Integer> {
