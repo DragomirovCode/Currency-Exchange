0747bc370ca6843d340bae3dbbe53ad7bb35584f
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-19 21:37:29 +0300

    chore: Изменил нейминг

---
+++ src/main/java/ru/dragomirov/dto/CalculationDTO.java
@@ -0,0 +1,17 @@
+package ru.dragomirov.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import ru.dragomirov.models.Currency;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class CalculationDTO {
+    private Currency baseCurrency;
+    private Currency targetCurrency;
+    private BigDecimal rate;
+    private BigDecimal amount;
+    private BigDecimal convertedAmount;
+}
