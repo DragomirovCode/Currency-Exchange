2acb111fd21d7b9d6f61b04ac7370ec397286ca5
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-19 22:43:47 +0300

    feat: Реализовал логику класса "MappingUtils"

--- src/main/java/ru/dragomirov/utils/MappingUtils.java
+++ src/main/java/ru/dragomirov/utils/MappingUtils.java
@@ -1,4 +1,24 @@
 package ru.dragomirov.utils;
 
+import org.modelmapper.ModelMapper;
+import ru.dragomirov.dto.CurrencyDTO;
+import ru.dragomirov.entities.Currency;
+
 public class MappingUtils {
+    private static final ModelMapper MODEL_MAPPER;
+
+    static {
+        MODEL_MAPPER = new ModelMapper();
+
+        MODEL_MAPPER.typeMap(CurrencyDTO.class, Currency.class)
+                .addMapping(CurrencyDTO::getName, Currency::setFullName);
+    }
+
+    public static Currency toEntity(CurrencyDTO currencyDTO) {
+        return MODEL_MAPPER.map(currencyDTO, Currency.class);
+    }
+
+    public static CurrencyDTO toDTO(Currency currency){
+        return MODEL_MAPPER.map(currency, CurrencyDTO.class);
+    }
 }
