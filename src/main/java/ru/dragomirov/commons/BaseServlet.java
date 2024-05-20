3744874f56b5aeea9c2a04ad6a00705a4869ffba
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-20 13:55:47 +0300

    chore(003): Обновил abstract class BaseServlet

--- src/main/java/ru/dragomirov/commons/BaseServlet.java
+++ src/main/java/ru/dragomirov/commons/BaseServlet.java
@@ -4,28 +4,26 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class BaseServlet extends HttpServlet {
-    protected void http400Errors(HttpServletResponse resp, String errorMessage) throws IOException{
-        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        String errorResponse = "{\"message\": \"" + errorMessage + "\"}";
-        resp.getWriter().write(errorResponse);
+    private Map<Integer, ErrorHandler> errorHandlers = new HashMap();
+
+    {
+        errorHandlers.put(400, new Http400ErrorHandler());
+        errorHandlers.put(404, new Http404ErrorHandler());
+        errorHandlers.put(409, new Http409ErrorHandler());
+        errorHandlers.put(500, new Http500ErrorHandler());
     }
-    protected void http404Errors(HttpServletResponse resp, String errorMessage) throws IOException{
-        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        String errorResponse = "{\"message\": \"" + errorMessage + "\"}";
-        resp.getWriter().write(errorResponse);
-    }
-    protected void http409Errors(HttpServletResponse resp, String errorMessage) throws IOException{
-        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        String errorResponse = "{\"message\": \"" + errorMessage + "\"}";
-        resp.getWriter().write(errorResponse);
-    }
-    protected void http500Errors(HttpServletResponse resp, Exception e, String errorMessage) throws IOException {
-        resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        String errorResponse = "{\"message\": \"" + errorMessage + "\"}";
-        resp.getWriter().write(errorResponse);
-        e.printStackTrace();
+
+    protected void handleError(int errorCode, HttpServletResponse resp, String errorMessage) throws IOException {
+        ErrorHandler errorHandler = errorHandlers.get(errorCode);
+        if (errorHandler != null){
+            errorHandler.httpErrors(resp, errorMessage);
+        } else {
+            System.err.println("Обработчик ошибок не найден для кода ошибки: " + errorCode);
+        }
     }
     protected BigDecimal parseBigDecimal(String string) {
         BigDecimal rate = null;
