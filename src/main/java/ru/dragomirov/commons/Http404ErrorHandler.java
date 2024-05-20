645658d5ee55f49e964ae176e5c163630c8e5307
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-20 13:46:02 +0300

    feat(003): Создал class Http404ErrorHandler и реализовал метод "httpErrors"

---
+++ src/main/java/ru/dragomirov/commons/Http404ErrorHandler.java
@@ -0,0 +1,14 @@
+package ru.dragomirov.commons;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Http404ErrorHandler implements ErrorHandler{
+    @Override
+    public void httpErrors(HttpServletResponse resp, String errorMessage) throws IOException {
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        String errorResponse = "{\"message\": \"" + errorMessage + "\"}";
+        resp.getWriter().write(errorResponse);
+    }
+}
