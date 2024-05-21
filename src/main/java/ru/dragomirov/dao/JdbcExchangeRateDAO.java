4c6747d565beb5e57a8cb4673ddc487dbd653e9b
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-22 01:19:03 +0300

    feat(005): реализовал метод "findByBaseCurrency" и "findByTargetCurrency"

--- src/main/java/ru/dragomirov/dao/JdbcExchangeRateDAO.java
+++ src/main/java/ru/dragomirov/dao/JdbcExchangeRateDAO.java
@@ -17,6 +17,8 @@ public class JdbcExchangeRateDAO implements ExchangeRateDAO {
     private static final String FIND_ALL_QUERY = "SELECT * FROM ExchangeRates";
     private static final String FIND_BY_ID_QUERY = "SELECT * FROM ExchangeRates WHERE id=?";
     private static final String FIND_BY_CURRENCY_PAIR_QUERY = "SELECT * FROM ExchangeRates WHERE base_currency_id=? AND target_currency_id=?";
+    private static final String FIND_BY_BASE_CURRENCY_QUERY = "SELECT * FROM ExchangeRates WHERE base_currency_id=?";
+    private static final String FIND_BY_TARGET_CURRENCY_QUERY = "SELECT * FROM ExchangeRates WHERE target_currency_id=?";
     private static final String SAVE_QUERY = "INSERT INTO ExchangeRates (base_currency_id, target_currency_id, rate) VALUES (?, ?, ?)";
     private static final String UPDATE_QUERY = "UPDATE ExchangeRates SET base_currency_id=?, target_currency_id=?, rate=? WHERE id=?";
     private static final String DELETE_QUERY = "DELETE FROM ExchangeRates WHERE id=?";
@@ -134,6 +136,40 @@ public class JdbcExchangeRateDAO implements ExchangeRateDAO {
             }
         } catch (SQLException e) {
             System.err.println("Произошла ошибка при выполнении метода 'findByCurrencyPair' (ExchangeRateDAO): " + e.getMessage());
+            return Optional.empty();
+        }
+        return Optional.ofNullable(exchangeRate);
+    }
+
+    @Override
+    public Optional<ExchangeRate> findByBaseCurrency(int baseCurrencyCode) {
+        ExchangeRate exchangeRate = null;
+        try (PreparedStatement statement = this.connection.prepareStatement(FIND_BY_BASE_CURRENCY_QUERY)) {
+            statement.setInt(1, baseCurrencyCode);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    exchangeRate = mapResultSetToExchangeRate(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Произошла ошибка при выполнении метода 'findByBaseCurrency' (ExchangeRateDAO): " + e.getMessage());
+            return Optional.empty();
+        }
+        return Optional.ofNullable(exchangeRate);
+    }
+
+    @Override
+    public Optional<ExchangeRate> findByTargetCurrency(int targetCurrencyCode) {
+        ExchangeRate exchangeRate = null;
+        try (PreparedStatement statement = this.connection.prepareStatement(FIND_BY_TARGET_CURRENCY_QUERY)) {
+            statement.setInt(1, targetCurrencyCode);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    exchangeRate = mapResultSetToExchangeRate(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Произошла ошибка при выполнении метода 'findByTargetCurrency' (ExchangeRateDAO): " + e.getMessage());
             return Optional.empty();
         }
         return Optional.ofNullable(exchangeRate);
