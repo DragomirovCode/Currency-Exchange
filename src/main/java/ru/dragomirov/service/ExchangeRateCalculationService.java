2eb2ed2fadaf769741fbed451a3c2840b2926aa7
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-21 17:51:37 +0300

    feat(005): перенёс бизнес-логику "ExchangeRateCalculationServlet" в "ExchangeRateCalculationService" для сценария AB

--- src/main/java/ru/dragomirov/service/ExchangeRateCalculationService.java
+++ src/main/java/ru/dragomirov/service/ExchangeRateCalculationService.java
@@ -1,4 +1,68 @@
 package ru.dragomirov.service;
 
+import ru.dragomirov.dao.JdbcCurrencyDAO;
+import ru.dragomirov.dao.JdbcExchangeRateDAO;
+import ru.dragomirov.dto.CalculationDTO;
+import ru.dragomirov.dto.CalculationDTOFactory;
+import ru.dragomirov.dto.CurrencyDTO;
+import ru.dragomirov.entities.Currency;
+import ru.dragomirov.entities.ExchangeRate;
+import ru.dragomirov.utils.BigDecimalUtils;
+import ru.dragomirov.utils.MappingUtils;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+/**
+ * ExchangeRateCalculationService реализует бизнес-логику ExchangeRateCalculationServlet
+ */
 public class ExchangeRateCalculationService {
+    private JdbcCurrencyDAO jdbcCurrencyDAO;
+    private JdbcExchangeRateDAO jdbcExchangeRateDAO;
+    private CalculationDTOFactory calculationDTOFactory;
+
+    public ExchangeRateCalculationService() {
+        this.jdbcCurrencyDAO = new JdbcCurrencyDAO();
+        this.jdbcExchangeRateDAO = new JdbcExchangeRateDAO();
+        this.calculationDTOFactory = new CalculationDTOFactory();
+    }
+
+    public CalculationDTO calculateExchangeRate(String fromCurrencyCode, String toCurrencyCode, String amountString) {
+        BigDecimal amount = parseAmount(amountString);
+        Currency fromCurrency = findCurrency(fromCurrencyCode);
+        Currency toCurrency = findCurrency(toCurrencyCode);
+        if (fromCurrency == null || toCurrency == null){
+            return null;
+        } else {
+            ExchangeRate exchangeRate = findExchangeRate(fromCurrency, toCurrency);
+            BigDecimal convertedAmount = calculateConvertedAmount(amount, exchangeRate);
+            return createCalculationDTO(amount, convertedAmount, exchangeRate);
+        }
+    }
+
+    private BigDecimal parseAmount(String amountString) {
+        return BigDecimalUtils.parseBigDecimal(amountString);
+    }
+
+    private Currency findCurrency(String currencyCode) {
+        Optional<Currency> currency = jdbcCurrencyDAO.findByCode(currencyCode);
+        return currency.orElse(null);
+    }
+
+    private ExchangeRate findExchangeRate(Currency fromCurrency, Currency toCurrency) {
+        Optional<ExchangeRate> exchangeRate = jdbcExchangeRateDAO.findByCurrencyPair(fromCurrency.getId(), toCurrency.getId());
+        return exchangeRate.orElse(null);
+    }
+
+    private BigDecimal calculateConvertedAmount(BigDecimal amount, ExchangeRate exchangeRate) {
+        return amount.multiply(exchangeRate.getRate());
+    }
+
+    private CalculationDTO createCalculationDTO(BigDecimal amount, BigDecimal convertedAmount, ExchangeRate exchangeRate) {
+        CurrencyDTO baseCurrency = MappingUtils.currencyToDTO(exchangeRate.getBaseCurrency());
+        CurrencyDTO targetCurrency = MappingUtils.currencyToDTO(exchangeRate.getTargetCurrency());
+        return calculationDTOFactory.createCalculationDTO(
+                baseCurrency, targetCurrency, exchangeRate.getRate(),
+                amount, convertedAmount);
+    }
 }
--- src/main/java/ru/dragomirov/servlets/ExchangeRateCalculationServlet.java
+++ src/main/java/ru/dragomirov/servlets/ExchangeRateCalculationServlet.java
@@ -1,37 +1,23 @@
 package ru.dragomirov.servlets;
 
-import ru.dragomirov.dao.JdbcCurrencyDAO;
-import ru.dragomirov.dao.JdbcExchangeRateDAO;
 import ru.dragomirov.dto.CalculationDTO;
-import ru.dragomirov.dto.CalculationDTOFactory;
-import ru.dragomirov.dto.CurrencyDTO;
-import ru.dragomirov.entities.Currency;
-import ru.dragomirov.entities.ExchangeRate;
 import com.google.gson.Gson;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import ru.dragomirov.utils.BigDecimalUtils;
-import ru.dragomirov.utils.MappingUtils;
+import ru.dragomirov.service.ExchangeRateCalculationService;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.Optional;
 
 /**
  * @doGet: Расчёт перевода определённого количества средств из одной валюты в другую.
  */
 @WebServlet(name = "ExchangeRateCalculationServlet", urlPatterns = "/exchange")
 public class ExchangeRateCalculationServlet extends HttpErrorHandlingServlet {
-    private JdbcExchangeRateDAO jdbcExchangeRateDAO;
-    private JdbcCurrencyDAO jdbcCurrencyDAO;
-    private CalculationDTOFactory calculationDTOFactory;
-
+    private ExchangeRateCalculationService exchangeRateCalculationService;
     @Override
     public void init() {
-        this.jdbcExchangeRateDAO = new JdbcExchangeRateDAO();
-        this.jdbcCurrencyDAO = new JdbcCurrencyDAO();
-        this.calculationDTOFactory = new CalculationDTOFactory();
+        this.exchangeRateCalculationService = new ExchangeRateCalculationService();
     }
 
     @Override
@@ -46,37 +32,14 @@ public class ExchangeRateCalculationServlet extends HttpErrorHandlingServlet {
                 return;
             }
 
-            BigDecimal amount = BigDecimalUtils.parseBigDecimal(amountString);
+            CalculationDTO calculationDTO = exchangeRateCalculationService.calculateExchangeRate(fromCurrencyCode, toCurrencyCode, amountString);
 
-            Optional<Currency> fromCurrency = jdbcCurrencyDAO.findByCode(fromCurrencyCode);
-            Optional<Currency> toCurrency = jdbcCurrencyDAO.findByCode(toCurrencyCode);
-
-            if (fromCurrency.isEmpty() || toCurrency.isEmpty()) {
+            if (calculationDTO == null){
                 handleError(404, resp, "Валютная пара отсутствует в базе данных");
                 return;
             }
-
-            int fromCurrencyId = fromCurrency.get().getId();
-            int toCurrencyId = toCurrency.get().getId();
-
-            Optional<ExchangeRate> exchangeRate = jdbcExchangeRateDAO.findByCurrencyPair(fromCurrencyId, toCurrencyId);
-
-            if (exchangeRate.isEmpty()) {
-                handleError(404, resp, "Валютная пара отсутствует в базе данных");
-                return;
-            }
-
-            BigDecimal convertedAmount = amount.multiply(exchangeRate.get().getRate());
-
-            CurrencyDTO baseCurrency = MappingUtils.currencyToDTO(exchangeRate.get().getBaseCurrency());
-            CurrencyDTO targetCurrency = MappingUtils.currencyToDTO(exchangeRate.get().getTargetCurrency());
-
-            CalculationDTO calculationDTO = calculationDTOFactory.createCalculationDTO(
-                    baseCurrency, targetCurrency, exchangeRate.get().getRate(),
-                    amount, convertedAmount);
 
             String jsonResponse = new Gson().toJson(calculationDTO);
-
             resp.getWriter().write(jsonResponse);
             resp.setStatus(HttpServletResponse.SC_OK);
         } catch (Exception e) {
