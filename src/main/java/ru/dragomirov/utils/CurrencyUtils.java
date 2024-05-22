7325f6625a091cb891197ebad5f2e748f461c1cd
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-22 19:09:06 +0300

    feat(006): добавил комментарий

--- src/main/java/ru/dragomirov/utils/CurrencyUtils.java
+++ src/main/java/ru/dragomirov/utils/CurrencyUtils.java
@@ -3,6 +3,9 @@ package ru.dragomirov.utils;
 import java.util.Currency;
 import java.util.Set;
 
+/**
+ * CurrencyUtils предоставляет метод для поиска валюты по ее коду и символу.
+ */
 public class CurrencyUtils {
     private final Set<Currency> currencySet = Currency.getAvailableCurrencies();
     public boolean searchCurrency(String code, String symbol) {
